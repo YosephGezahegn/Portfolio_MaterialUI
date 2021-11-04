ced67f9cacc05fce4da993fb13e7b2fca9057dc5
Yoseph <beyoabde@gmail.com>
2021-11-04 15:15:02 +0200

    pic added

--- src/Info/Experience.js
+++ src/Info/Experience.js
@@ -1,37 +1,33 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React from "react";
->>>>>>> 11d6b035a64abd421de3ac9b3cd28535bdceac8f
 
 const Experience = () => {
-  return (
-    <div>
-      <h2>2012 - 2015 </h2>
+	return (
+		<div>
+			<h2>2012 - 2015 </h2>
 
-      <h2> Ethiopian Airlines</h2>
-      <h3> Cargo Systems and Handling Agent </h3>
-      <hr style={{ borderTop: "3px solid #e22947" }} />
-      <ul>
-        <li>
-          {" "}
-          Loading on cargo spot system for all passenger and cargo flights.
-        </li>
-        <li>
-          Check with offloading file and plan shipments for the next available
-          flights.
-        </li>
-        <li>
-          Check the availability of documents with corresponding shipments .
-        </li>
-        <li>
-          Load preparation by considering, Aircraft type, booking, nature
-          shipment.
-        </li>
-      </ul>
-      <hr style={{ borderTop: "3px solid #e22947" }} />
-    </div>
-  );
+			<h2> Ethiopian Airlines</h2>
+			<h3> Cargo Systems and Handling Agent </h3>
+			<hr style={{ borderTop: '3px solid #e22947' }} />
+			<ul>
+				<li>
+					{' '}
+					Loading on cargo spot system for all passenger and cargo flights.
+				</li>
+				<li>
+					Check with offloading file and plan shipments for the next available
+					flights.
+				</li>
+				<li>
+					Check the availability of documents with corresponding shipments .
+				</li>
+				<li>
+					Load preparation by considering, Aircraft type, booking, nature
+					shipment.
+				</li>
+			</ul>
+			<hr style={{ borderTop: '3px solid #e22947' }} />
+		</div>
+	);
 };
 
 export default Experience;
